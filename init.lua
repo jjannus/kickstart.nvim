73d0829457191b4045f61fa966fe0f546b1fc7b8
Jan Hegi <jan@jhegi.de>
2026-05-11 11:59:52 +0200

    Add prettier for typescript

--- init.lua
+++ init.lua
@@ -704,8 +704,9 @@ require('lazy').setup({
       formatters_by_ft = {
         -- rust = { 'rustfmt' },
         -- Conform can also run multiple formatters sequentially
-        python = { "isort", "black" },
-        sh = { "shellcheck", "shfmt" },
+        typescript = { 'prettier' },
+        python = { 'isort', 'black' },
+        sh = { 'shellcheck', 'shfmt' },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
