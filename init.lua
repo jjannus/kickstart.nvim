9fbdeb30be5d8300ef9866e72908ae76907df34e
Jan Hegi <jan@jhegi.de>
2026-09-12 19:24:20 +0200

    Set linebreak option in init.lua

--- init.lua
+++ init.lua
@@ -171,6 +171,8 @@ do
   -- instead raise a dialog asking if you wish to save the current file(s)
   -- See `:help 'confirm'`
   vim.o.confirm = true
+
+  vim.opt.linebreak = true
 end
 
 -- ============================================================
